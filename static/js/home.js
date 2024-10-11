d7d43c0678d0d04e738c4937800f1158e6413057
KPCOFGS <shixian_sheng-2@protonmail.com>
2024-10-11 15:42:53 -0400

    Revert "Update home.js"
    This reverts commit 08da343ba03bfe223bf54b6a0fff86f15fc8d672.

--- static/js/home.js
+++ static/js/home.js
@@ -2,7 +2,6 @@ const toggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 const header = document.querySelector('header');
 const sections = document.querySelectorAll('.section');
-const footer = document.querySelector('footer'); //Include footer information
 
 // Check user preference and set the theme
 const currentTheme = localStorage.getItem('theme') || 'light';
@@ -10,7 +9,6 @@ if (currentTheme === 'dark') {
     body.classList.add('dark');
     header.classList.add('dark');
     sections.forEach(section => section.classList.add('dark'));
-    footer.classList.add('dark'); //Add the dark mode capability to the footer 
     toggleButton.textContent = 'Toggle Light Mode';
 }
 
@@ -19,7 +17,6 @@ toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
     header.classList.toggle('dark');
     sections.forEach(section => section.classList.toggle('dark'));
-    footer.classList.toggle('dark'); //Enable the dark mode class for the footer
 
     const isDarkMode = body.classList.contains('dark');
     localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
